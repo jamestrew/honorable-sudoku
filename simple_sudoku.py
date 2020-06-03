2346d28bddf6dd1fddff23878208d637221f2891
Eugene Min <mineugene96@gmail.com>
2020-06-03 05:26:15 -0400

    - factored out solution var + updated format using new model * global puzzle update fixed

--- simple_sudoku.py
+++ simple_sudoku.py
@@ -6,22 +6,24 @@ BOARD SOLUTION: https://i.imgur.com/T4hYPQx.png
 
 """
 
+from sudoku import Puzzle
+
 
 # setting up an easy game board
 # maybe add other options/difficulties later
 easy = [
-    [5,0,0,8,3,2,4,0,6],
-    [0,6,3,7,4,0,0,0,0],
-    [8,0,2,1,9,0,0,0,3],
-    [0,3,0,0,2,9,1,0,5],
-    [1,0,0,0,0,8,9,6,2],
-    [0,0,0,5,0,0,0,7,0],
-    [0,0,0,0,1,0,0,2,7],
-    [0,2,6,0,0,0,5,0,0],
-    [3,1,8,0,5,0,0,4,0]
+    5, 0, 0, 8, 3, 2, 4, 0, 6,
+    0, 6, 3, 7, 4, 0, 0, 0, 0,
+    8, 0, 2, 1, 9, 0, 0, 0, 3,
+    0, 3, 0, 0, 2, 9, 1, 0, 5,
+    1, 0, 0, 0, 0, 8, 9, 6, 2,
+    0, 0, 0, 5, 0, 0, 0, 7, 0,
+    0, 0, 0, 0, 1, 0, 0, 2, 7,
+    0, 2, 6, 0, 0, 0, 5, 0, 0,
+    3, 1, 8, 0, 5, 0, 0, 4, 0
 ]
 
-game = easy
+game = Puzzle(easy)
 
 # print the board semi-cleanly
 def print_board(game):
@@ -29,20 +31,21 @@ def print_board(game):
         print(" ".join(map(str,row)))
 
 print("Starting Board:")
-print_board(game)
-print("\n")
+#print_board(game)
+print(game)
+print('\n')
 
-# queries user for playmode
+# queries user for game mode
 #auto_solve = input("\n\nThis is a gay game.\n 1. Play\n 2. Auto solve\n")
 
 # checks whether the move is legal
 def is_possible(x, y, n):
     global game
     for i in range(9):
-        if game[x][i] == n:
+        if game[x, i] == n:
             return False
     for i in range(9):
-        if  game[i][y] == n:
+        if game[i, y] == n:
             return False
 
     x0 = (x//3)*3
@@ -50,7 +53,7 @@ def is_possible(x, y, n):
 
     for i in range(x0, x0+3):
         for j in range(y0, y0+3):
-            if game[i][j] == n:
+            if game[i, j] == n:
                 return False
     return True
 
@@ -58,42 +61,38 @@ def is_possible(x, y, n):
 def is_playball(game):
     for i in range(9):
         for j in range(9):
-            if game[i][i] == 0:
+            if game[i, i] == 0:
                 return True
 
 # solves sudoko, runs regardless of player choice
 solution = []
-def solve():
-    global game, solution
+def solve(game):
     for x in range(9):
         for y in range(9):
-            if game[x][y] == 0:
+            if game[x, y] == 0:
                 for n in range(1,10):
                     if is_possible(x, y, n):
-                        game[x][y] = n
-                        solve()
-                        game[x][y] = 0
+                        game[x, y] = n
+                        solve(game)
                 return
-    print_board(game)
-    print("\n")
-    if game[8][8] != 0: # what the fuck
-        print(game[8][8])
-        solution = game
+    #if game[8, 8] != 0: # what the fuck
+    #    print(game[8, 8])
+    #    solution = game
 
 
-solve()
-auto_solve = 2 # temporary
+solve(game)
+auto_solve = 2  # temporary
 if auto_solve == 2:
     print("\nSolution:")
-    print_board(solution)
-elif auto_solve == 1: # user plays
-    while is_playball(easy_game):
+    print(game)
+elif auto_solve == 1:  # user plays
+    while is_playball(game):
         x = int(input("Enter x coordinate: "))
         y = int(input("Enter y coordinate: "))
         n = int(input("Enter number to enter: "))
 
-        if solution[x][y] == n:
-            game[x][y] = n
-            print_board(game)
+        if solution[x, y] == n:
+            game[x, y] = n
+            print(game)
         else:
             print("Try again")

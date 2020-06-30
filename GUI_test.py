55eb73360e08d578e17bbc02a7485c1e488e07d0
trewjames <j.trew10@gmail.com>
2020-06-29 20:33:36 -0400

    create border thicknesses

--- GUI_test.py
+++ GUI_test.py
@@ -22,12 +22,22 @@ class Game(tk.Frame):
         self.cells = []
         for i in range(9):
             row = []
+            yborder = 0.5
+            if i % 3 == 0 and i != 0:
+                yborder = 5
             for j in range(9):
+                xborder = 0.5
+                if j % 3 == 0 and j != 0:
+                    xborder = 5
+
                 cell_frame = tk.Frame(self.main_grid, width=75, height=75, bg="#FFFFFF")
-                cell_frame.grid(row=i, column=j, padx=border_thickness, pady=border_thickness)
+                cell_frame.grid(row=i, column=j,
+                                padx=(xborder, 0.5),
+                                pady=(yborder, 0.5)
+                                )
                 cell_number = tk.Label(self.main_grid, bg="#FFFFFF", fg="#000000",
-                                       text='5',
-                                       font=("Helvetica", "30", "bold"),
+                                       text=f"{i},{j}",
+                                       font=("Helvetica", "20", "bold"),
                                        )
                 cell_number.grid(row=i, column=j)
                 cell_data = {"frame": cell_frame, "number": cell_number}
@@ -36,9 +46,7 @@ class Game(tk.Frame):
 
         # make 'scoresheet'
         score_frame_top = tk.Frame(self)
-        score_frame_bottom = tk.Frame(self)
         score_frame_top.place(relx=0.5, y=850, anchor="center")
-        score_frame_bottom.place(relx=0.5, y=950, anchor="center")
         for i in range(1, 6):
             cell_score_top = tk.Frame(score_frame_top, width=75, height=75, bg="#f2e8cb")
             cell_score_top.grid(row=0, column=i, padx=10, pady=10)
@@ -48,8 +56,12 @@ class Game(tk.Frame):
                                        )
             cell_number_top.grid(row=0, column=i)
 
+        score_frame_bottom = tk.Frame(self)
+        score_frame_bottom.place(relx=0.5, y=950, anchor="center")
         for i in range(6, 10):
-            cell_score_bottom = tk.Frame(score_frame_bottom, width=75, height=75, bg="#f2e8cb")
+            cell_score_bottom = tk.Frame(score_frame_bottom, width=75, height=75,
+                                         bg="#f2e8cb"
+                                         )
             cell_score_bottom.grid(row=1, column=i - 5, padx=10, pady=10)
             cell_number_bottom = tk.Label(score_frame_bottom, bg="#f2e8cb", fg="#000000",
                                           text=i,

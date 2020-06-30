53a8915c21b58d0ac27dbe9603c71db977142b68
trewjames <j.trew10@gmail.com>
2020-06-30 15:23:08 -0400

    add start screen

--- GUI_test.py
+++ GUI_test.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from PIL import Image
 
 
 class Game(tk.Frame):
@@ -12,9 +13,88 @@ class Game(tk.Frame):
                                   bg="#000000", bd=5
                                   )
         self.main_grid.grid(padx=75, pady=(75, 225))
-        self.make_GUI()
+        self.start_GUI()
+        # self.make_GUI()
 
         self.mainloop()
+
+    def start_GUI(self):
+        start_frame = tk.Frame(self.main_grid, width=675, height=675, bg="#FFFFFF")
+        start_frame.grid(row=0, column=0)
+        title_label = tk.Label(start_frame, text='GAYDOKU',
+                               bg="#FFFFFF", fg="#000000",
+                               font=("Helvetica", "55", "bold")
+                               )
+        title_label.grid(row=0, padx=10, pady=10)
+
+        # gamemode select
+        self.gamemode = 0
+        mode_frame = tk.Frame(start_frame, width=675, height=150, bg="#FFFFFF")
+        mode_frame.grid(row=1, padx=10, pady=10)
+        tk.Label(mode_frame, text="MODE: ", bg="#FFFFFF", fg="#000000",
+                 font=("Helvetica", "30", "bold")
+                 ).grid()
+
+        def gamemode(mode):
+            if mode == 1:
+                computer_button.configure(relief='sunken')
+                user_button.configure(relief='raised')
+            else:
+                computer_button.configure(relief='raised')
+                user_button.configure(relief='sunken')
+            self.gamemode = mode
+
+        computer_button = tk.Button(mode_frame, text='COMP PLAY', bg="#FFFFFF", fg="#000000",
+                                    font=("Helvetica", "25", "bold"),
+                                    command=lambda: gamemode(1)
+                                    )
+        user_button = tk.Button(mode_frame, text='USER PLAY', bg="#FFFFFF", fg="#000000",
+                                font=("Helvetica", "25", "bold"),
+                                command=lambda: gamemode(2)
+                                )
+        computer_button.grid(row=0, column=1, padx=5, pady=5)
+        user_button.grid(row=0, column=2, padx=5, pady=5)
+
+        # difficulty select
+        self.difficulty = 0
+        diff_frame = tk.Frame(start_frame, width=675, height=150, bg="#FFFFFF")
+        diff_frame.grid(row=2, padx=10, pady=10)
+        tk.Label(diff_frame, text="DIFFICULTY: ", bg="#FFFFFF", fg="#000000",
+                 font=("Helvetica", "30", "bold")
+                 ).grid()
+
+        def difficulty(diff):
+            easy_button.configure(relief='raised')
+            hard_button.configure(relief='raised')
+            magic_button.configure(relief='raised')
+
+            if diff == 1:
+                easy_button.configure(relief='sunken')
+            elif diff == 2:
+                hard_button.configure(relief='sunken')
+            else:
+                magic_button.configure(relief='sunken')
+
+            self.difficulty = diff
+            print(self.difficulty)
+
+        easy_button = tk.Button(diff_frame, text='EASY', bg="#FFFFFF", fg="#000000",
+                                font=("Helvetica", "25", "bold"),
+                                command=lambda: difficulty(1)
+                                )
+        hard_button = tk.Button(diff_frame, text='HARD', bg="#FFFFFF", fg="#000000",
+                                font=("Helvetica", "25", "bold"),
+                                command=lambda: difficulty(2)
+                                )
+        magic_button = tk.Button(diff_frame, text='MAGIC', bg="#FFFFFF", fg="#000000",
+                                 font=("Helvetica", "25", "bold"),
+                                 command=lambda: difficulty(3)
+                                 )
+        easy_button.grid(row=0, column=1, padx=5, pady=5)
+        hard_button.grid(row=0, column=2, padx=5, pady=5)
+        magic_button.grid(row=0, column=3, padx=5, pady=5)
+
+        # icon
 
     def make_GUI(self):
 

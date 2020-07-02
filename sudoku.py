6529ddacba248ed5054c3434e805c41c4bce561a
trewjames <j.trew10@gmail.com>
2020-07-01 21:14:31 -0400

    format to PEP8 compliance

--- sudoku.py
+++ sudoku.py
@@ -4,6 +4,8 @@ from functools import reduce
 
 
 """ Sudoku::MODEL """
+
+
 class Puzzle(object):
     # grid: Z^1 array for board
     __grid = []
@@ -12,22 +14,21 @@ class Puzzle(object):
     (Default/Copy) constructor
         - Takes an optional non-positional argument of type Puzzle to copy
     """
+
     def __init__(self, grid=None, /):
         if grid is None:  # fill empty grid
-            self.__grid = [0]*(DIM**2)
+            self.__grid = [0] * (DIM**2)
         elif isinstance(grid, Puzzle):  # copy init
             pass
         elif isinstance(grid, list):    # copy init
-            self.__grid = [cp%(DIM+1) for cp in grid]
+            self.__grid = [cp % (DIM + 1) for cp in grid]
         else:
-            raise TypeError\
-            (
+            raise TypeError(
                 f"expected {type(self)} or {type([])}, but found {type(grid)}"
             )
         if not self.__validate():
             del self  # that's unfortunate
-            raise ValueError\
-            (
+            raise ValueError(
                 f"validation failed."
             )
 
@@ -37,10 +38,10 @@ class Puzzle(object):
                          for (i) in range(0, len(self.__grid), DIM))
 
     def __getitem__(self, key):
-        return self.__grid[key[0]*9 + key[1]]
+        return self.__grid[key[0] * 9 + key[1]]
 
     def __setitem__(self, key, val):
-        self.__grid[key[0]*9 + key[1]] = val
+        self.__grid[key[0] * 9 + key[1]] = val
 
     # METHODS ( PRIVATE ):
     '''
@@ -50,13 +51,15 @@ class Puzzle(object):
         - Each cell value is of <class: 'int'>
         - BLKS,COLS,ROWS are composed of unique integers (excluding zero/empty)
     '''
+
     def __validate(self):
         try:
-            if len(self.__grid) != DIM*DIM: raise ValueError
+            if len(self.__grid) != DIM * DIM:
+                raise ValueError
             grid = list(map(int, self.__grid))
-        except (TypeError, ValueError) as err:
+        except (TypeError, ValueError):
             return False
-        grid= list(map(int, grid))
+        grid = list(map(int, grid))
         result = []  # distinct-check flags
         for i in range(DIM):
             v_nbr = self.__vlookup(i)
@@ -64,9 +67,9 @@ class Puzzle(object):
             b_nbr = self.__blookup(i)
             result.append(
                 # |{non-empty cells}| == |{unique cells}\{0}|
-                len(list(filter(lambda n: n!=0, v_nbr))) == len(set(v_nbr))-1 and
-                len(list(filter(lambda n: n!=0, h_nbr))) == len(set(h_nbr))-1 and
-                len(list(filter(lambda n: n!=0, b_nbr))) == len(set(b_nbr))-1
+                len(list(filter(lambda n: n != 0, v_nbr))) == len(set(v_nbr)) - 1 and \
+                len(list(filter(lambda n: n != 0, h_nbr))) == len(set(h_nbr)) - 1 and \
+                len(list(filter(lambda n: n != 0, b_nbr))) == len(set(b_nbr)) - 1
             )
         return all(result)
 
@@ -74,6 +77,7 @@ class Puzzle(object):
     vlookup(index) returns a ROW at the specified ROW-index.
         index follows 0..DIM convention (LHS to RHS)
     '''
+
     def __vlookup(self, index, /):
         return self.__grid[index:: DIM]
 
@@ -81,17 +85,19 @@ class Puzzle(object):
     hlookup(index) returns a COL at the specified COL-index.
         index follows 0..DIM convention (TOP to BOT)
     '''
+
     def __hlookup(self, index, /):
-        return self.__grid[index*DIM: DIM*(index+1)]
+        return self.__grid[index * DIM: DIM * (index + 1)]
 
     '''
     blookup(index) returns a BLK at the specified BLK-index.
         index follows 0..DIM convention (TOP-LHS to BOT-RHS)
     '''
+
     def __blookup(self, index, /):
         blk_nbr = (
-            self.__grid[k*DIM//3: DIM//3*(k+1)]
-            for k in range(index, (2*DIM//3+index)+1, DIM//3)
+            self.__grid[k * DIM // 3: DIM // 3 * (k + 1)]
+            for k in range(index, (2 * DIM // 3 + index) + 1, DIM // 3)
         )
         return reduce(operator.concat, blk_nbr, [])
 
@@ -103,14 +109,16 @@ class Puzzle(object):
         - (int)index is in [0..DIM-1]
         - (int)lookup is in [0..2] or [BLK,COL,ROW]
     '''
+
     def neighbor(self, index, lookup=None):
         # ASSERTS:
-        if index<0 or index>=DIM:
+        if index < 0 or index >= DIM:
             raise ValueError
         if lookup and not(isinstance(lookup, int)):
             raise TypeError
 
-        if not lookup: lookup = BLK   # default
+        if not lookup:
+            lookup = BLK   # default
         return {
             BLK: lambda idx: self.__blookup(idx),
             COL: lambda idx: self.__vlookup(idx),

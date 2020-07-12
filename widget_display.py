7c70571ebf937dd2338e6af65605d1b1c4b75dc3
Eugene Min <mineugene96@gmail.com>
2020-07-11 23:02:01 -0400

    removed redundant function

--- widget_display.py
+++ widget_display.py
@@ -36,24 +36,6 @@ class WidgetDisplay(tk.Tk, View):
     def notify(self, x, y, val, /):
         print(f"[Debug] {type(self)}: successfully changed (col:{x}, row:{y}) to {val}.")
 
-    def set_gamemode(self):
-        """
-        set_gamemode() is a TEMPORARY method (to be removed)
-            A request is sent to the controller to update the gamemode.
-
-        IMPORTANT:
-            The button is updated instantly, but should not be updated here!
-            Request methods in Controller should return bool indiciating successful update, and
-            should only update view after ensuring True is returned (kinda like server requests).
-
-            This applies only if the data is tied to the Model or Controller states.
-            self.__accumulator is not tied to either; thus, it may be updated here directly.
-            Upon receiving False, the View may need to revert to an earlier state.
-        """
-        self.__accumulator += 1
-        self.maindiv.winfo_children()[0].configure(text=self.__accumulator)  # dangerous update
-        self.__request.gamemode_update(self.__accumulator)
-
 
 class Startup(tk.Frame, View):
     '''
